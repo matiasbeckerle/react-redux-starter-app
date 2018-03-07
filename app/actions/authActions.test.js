b0b5813aec9365bca8c090fcdae1bc3d4e69db2e
matiasbeckerle <matiasbeckerle@gmail.com>
2018-03-07 20:02:41 -0300

    WIP

--- app/actions/authActions.test.js
+++ app/actions/authActions.test.js
@@ -1,5 +1,10 @@
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
 import * as authActions from './authActions';
 import * as types from '../constants/actionTypes';
+
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
 
 describe('authActions', () => {
   it('should create an action to start a login request', () => {
@@ -36,7 +41,6 @@ describe('authActions', () => {
   it('should create an action to handle a failed login', () => {
     // Arrange.
     const message = 'Testing an error.';
-
     const expectedAction = {
       type: types.LOGIN_ERROR,
       message
@@ -47,5 +51,42 @@ describe('authActions', () => {
 
     // Assert.
     expect(result).toEqual(expectedAction);
+  });
+
+  it('should handle successful login when valid credentials provided', () => {
+    const username = 'username';
+    const password = 'password';
+    const user = {
+      name: 'John'
+    };
+
+    const store = mockStore({
+      auth: {}
+    });
+
+    // TODO: Mock service response.
+
+    return store.dispatch(authActions.login(username, password)).then(() => {
+      const actions = store.getActions();
+      expect(actions[0]).toEqual({ type: types.LOGIN_REQUEST });
+      expect(actions[1]).toEqual({ type: types.LOGIN_SUCCESS, user });
+    });
+  });
+
+  it('should handle failed login when invalid credentials provided', () => {
+    const username = 'invalid';
+    const password = 'invalid';
+    const message = 'Invalid credentials.';
+    const store = mockStore({
+      auth: {}
+    });
+
+    // TODO: Mock service response.
+
+    return store.dispatch(authActions.login(username, password)).then(() => {
+      const actions = store.getActions();
+      expect(actions[0]).toEqual({ type: types.LOGIN_REQUEST });
+      expect(actions[1]).toEqual({ type: types.LOGIN_ERROR, message });
+    });
   });
 });
